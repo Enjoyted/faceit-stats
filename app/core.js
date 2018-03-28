12849c4fe7c3f1499cad9ce124f5c302813d5b63
Teddy Isidor <teddy.isidor@ubicast.eu>
2018-03-28 17:15:44 +0200

    changed the way users are stored in the data map, so that it maps correctly users even if they have changed nicknames in the past. This prevents the same user to appear twice (or more depending on times he changed nickname) in the list.

--- app/core.js
+++ app/core.js
@@ -12,40 +12,39 @@
 			matches_api_url: "https://api.faceit.com/stats/api/v1/stats/time/users/",
 			nickname_api_url: "https://api.faceit.com/api/nicknames/",
 			current_user: null,
-			min_matches: 3,
+			min_matches: 5,
 			progress: 0,
 			_findFaction: function(json) {
-			  var faction = "faction";
-			  for (var ii = 0; ii < json[faction + "1"].length; ii++) {
-				if (json[faction + "1"][ii].nickname == this.current_user) {
-				  faction += "1";
-				  break;
+				var faction = "faction";
+				for (var ii = 0; ii < json[faction + "1"].length; ii++) {
+					if (json[faction + "1"][ii].nickname == this.current_user) {
+						faction += "1";
+						break;
+					}
 				}
-			  }
-			  if (faction.substr(-1, faction.length) === "n")
-				faction += "2";
-			  return faction;
+				if (faction.substr(-1, faction.length) === "n")
+					faction += "2";
+				return faction;
 			},
 			_fillData: function (datas, json, faction, winner) {
-			  for (var y=0;y<json[faction].length;y++) {
-				var user = json[faction][y].nickname;
-				if (user !== this.current_user) {
-				  if (user in datas) {
-				  	var totalGames = Number(datas[user].wins) + Number(datas[user].losses);
-					if (winner === faction) {
-					  datas[user].wins++;
-					  datas[user].percentage = Math.floor((datas[user].wins / (totalGames + 1)) * 100);
-					} else {
-					  datas[user].losses++;
-					  datas[user].percentage = Math.floor((datas[user].wins / (totalGames + 1)) * 100);
+				var self = this;
+				for (var y=0;y<json[faction].length;y++) {
+					var nickname = json[faction][y].nickname;
+					var guid = json[faction][y].guid;
+					if (guid !== self.guid) {
+						if (guid in datas) {
+							var totalGames = Number(datas[guid].wins) + Number(datas[guid].losses);
+							if (winner === faction)
+								datas[guid].wins++;
+							else
+								datas[guid].losses++;
+							datas[guid].percentage = Math.floor((datas[guid].wins / (totalGames + 1)) * 100);
+						} else {
+							datas[guid] = {"wins": (winner === faction ? 1 : 0), "losses": (winner !== faction ? 1 : 0), "nickname": nickname};
+						}
 					}
-				  }
-				  else {
-					datas[user] = {"wins": (winner === faction ? 1 : 0), "losses": (winner !== faction ? 1 : 0)};
-				  }
 				}
-			  }
-			  return datas;
+				return datas;
 			},
 			getData: function (matches) {
 				var self = this;
@@ -74,7 +73,7 @@
 						var datas = [];
 						for (var i in map) {
 							datas.push({
-								username: i,
+								nickname: map[i].nickname,
 								data: map[i]
 							});
 						}
@@ -90,7 +89,7 @@
 				console.log("calling " + self.nickname_api_url + self.current_user + " ...");
 				return (new Promise(function(resolve, reject) {
 					$http.get(self.nickname_api_url + self.current_user).then(function(response) {
-						console.log("user guid : ", response.data.payload.guid);
+						self.guid = response.data.payload.guid;
 						resolve(response.data.payload.guid);
 					}, function(error) {
 						// 404 user not found
@@ -111,8 +110,7 @@
 							if (json.length === 0) {
 								service.progress = 50;
 								resolve(arr);
-							}
-							else {
+							} else {
 								arr = arr.concat(json);
 								return (run(i + 1));
 							}
@@ -123,7 +121,8 @@
 					return (run(0));
 				}));
 			},
-			fetch: function(nickname) {
+			fetch: function(nickname, guid) {
+				var self = this;
 				var user_hash = "";
 				this.current_user = nickname;
 				return (new Promise(function(resolve, reject) {
@@ -161,14 +160,14 @@
 		$scope.user = {};
 		$scope.filter = {
 			username: '',
-			orderBy: 'username',
+			orderBy: 'nickname',
 			orderMode: '+',
 			minMatches: $scope.service.min_matches,
 		};
 
 		$scope.fetch = function(user) {
 			console.log("user 1 : ", user);
-			var matches = scrap.fetch(user.nickname).then(function (datas) {
+			var matches = scrap.fetch(user.nickname, user.guid).then(function (datas) {
 				$timeout(function () {
 			        $scope.$apply(function () {
 			            $scope.data = datas;
