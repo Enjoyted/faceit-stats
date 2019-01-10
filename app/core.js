be732e0cd1088ccf7880133f32319fe1c06fb189
enjoyted <enjoyted@gmail.com>
2019-01-10 20:07:29 +0100

    added map affinity data, excluded some type of unwanted games that were taken into account, enhanced progress bar to reflect on progression better, fixed error handling

--- app/core.js
+++ app/core.js
@@ -5,7 +5,7 @@
 */
 
 (function() {
-	var app = angular.module("app", []);
+	var app = angular.module("app", ["ngSanitize"]);
 
 	app.factory("scrap", ["$http", "$q", function($http, $q) {
 		var service = {
@@ -39,22 +39,76 @@
 				}
 				return faction;
 			},
+			_purgeNonElligibleGames:  function(old, json) {
+				/* Basically ignore non 5v5 games, non elo games */
+				var elligibleGame = false;
+				if (old) {
+					if (json.hasOwnProperty("match_type") && json.match_type.substr(0, 3) === "5v5") {
+						if (json.hasOwnProperty("updated_elo_rank") && json.updated_elo_rank === true)
+							elligibleGame = true
+						if  (json.hasOwnProperty("calculate_elo") && json.calculate_elo === true)
+							elligibleGame = true
+					}
+				} else {
+					if (json.entity.name.substr(0, 9) === "CS:GO 5v5" && json.calculateElo === true)
+						elligibleGame = true
+				}
+				return elligibleGame;
+			},
+			_getMapOfMatch: function(old, json) {
+				var map = "";
+				if (old) {
+					map = (json.hasOwnProperty("voted_entities")) ? json.voted_entities[0].map.guid : "";
+				} else {
+					map = (json.hasOwnProperty("voting")) ? json.voting.map.pick[0] : "";
+				}
+				return map;
+			},
 			_fillData: function (datas, json, faction, winner, old) {
 				var self = this;
 				var teams = (old) ? json[faction] : json[faction].roster ;
 				for (var y=0;y<teams.length;y++) {
 					var nickname = teams[y].nickname;
 					var guid = old ? teams[y].guid : teams[y].id;
+					var map = service._getMapOfMatch(old, json);
 					if (guid !== self.guid) {
 						if (guid in datas) {
 							var totalGames = Number(datas[guid].wins) + Number(datas[guid].losses);
-							if (winner === faction)
+							if (winner === faction) {								
 								datas[guid].wins++;
-							else
+								if (map !== "") {
+									datas[guid].maps[map].wins++;
+								}
+							}
+							else {
 								datas[guid].losses++;
+								if (map !== "") {
+									datas[guid].maps[map].losses++;
+								}
+							}
 							datas[guid].percentage = Math.floor((datas[guid].wins / (totalGames + 1)) * 100);
+							if (map !== "")
+								datas[guid].maps[map].percentage = Math.floor((datas[guid].maps[map].wins / (Number(datas[guid].maps[map].wins) + Number(datas[guid].maps[map].losses))) * 100)
 						} else {
-							datas[guid] = {"wins": (winner === faction ? 1 : 0), "losses": (winner !== faction ? 1 : 0), "nickname": nickname};
+							datas[guid] = {
+								"wins": (winner === faction ? 1 : 0),
+								"losses": (winner !== faction ? 1 : 0),
+								"nickname": nickname,
+								"maps" : {
+									"de_inferno": {"wins": 0, "losses": 0},
+									"de_dust2": {"wins": 0, "losses": 0},
+									"de_mirage": {"wins": 0, "losses": 0},
+									"de_train": {"wins": 0, "losses": 0},
+									"de_overpass": {"wins": 0, "losses": 0},
+									"de_nuke": {"wins": 0, "losses": 0},
+									"de_cache": {"wins": 0, "losses": 0},
+									"de_cbble": {"wins": 0, "losses": 0}
+								},
+							};
+							if (map !== "") {
+								if (datas[guid].maps.hasOwnProperty(map))
+									(winner === faction) ? datas[guid].maps[map].wins++ : datas[guid].maps[map].losses++;
+							}
 						}
 					}
 				}
@@ -67,8 +121,6 @@
 					var progress = 0;
 					var wait = [];
 					var loopMatches = function(i) {
-
-
 						var old = (matches[i].matchId.substr(0,2) == "1-") ? false : true;
 						var correct_match_api = old ? self.old_match_api_url : self.match_api_url;  
 
@@ -76,11 +128,13 @@
 							return $http.get(correct_match_api + matches[i].matchId + "?withStats=true").then(function (response) {
 								console.log("(" + i + ")" + "getting stats on OLD match : " + matches[i].matchId, service.progress);
 								var json = response.data.payload;
-								var winner = json.winner;
-								var faction = service._findFaction(json, old);
-								service.progress = (((progress * 40) / matches.length) + 60);
-								progress++;
-								return (service._fillData(map, json, faction, winner, old));
+								if (service._purgeNonElligibleGames(old, json)) {
+									var winner = json.winner;
+									var faction = service._findFaction(json, old);
+									service.progress = (((progress * 40) / matches.length) + 60);
+									progress++;
+									return (service._fillData(map, json, faction, winner, old));
+								}
 							}, function(error) {
 								$q.reject(error);
 							});
@@ -88,11 +142,13 @@
 							return $http.get(correct_match_api + matches[i].matchId).then(function (response) {
 								console.log("(" + i + ")" + "getting stats on match : " + matches[i].matchId, service.progress);
 								var json = response.data.payload;
-								var winner = json.results[0].winner;
-								var faction = service._findFaction(json.teams, old);
-								service.progress = (((progress * 40) / matches.length) + 60);
-								progress++;
-								return (service._fillData(map, json.teams, faction, winner, old));
+								if (service._purgeNonElligibleGames(old, json)) {
+									var winner = json.results[0].winner;
+									var faction = service._findFaction(json.teams, old);
+									service.progress = (((progress * 40) / matches.length) + 60);
+									progress++;
+									return (service._fillData(map, json.teams, faction, winner, old));
+								}
 							}, function(error) {
 								$q.reject(error);
 							});
@@ -133,11 +189,12 @@
 			getUserMatches: function (user_hash) {
 				var self = this;
 				var arr = [];
-
+				service.progress = 20;
 				return (new Promise(function(resolve, reject) {
 					var run = function(i) {
 						console.log(self.matches_api_url + user_hash + "/games/csgo?page=" + i + "&size=100");
 						return $http.get(self.matches_api_url + user_hash + "/games/csgo?page=" + i + "&size=100").then(function(response) {
+							service.progress = service.progress + 0.3
 							console.log("response : ", response);
 							var json = response.data;
 							if (json.length === 0) {
@@ -157,6 +214,8 @@
 			fetch: function(nickname, guid) {
 				var self = this;
 				var user_hash = "";
+				service.errormsg = false;
+				service.progress = 10;
 				this.current_user = nickname;
 				return (new Promise(function(resolve, reject) {
 					service.getUserHash().then(function (user_hash) {
@@ -164,13 +223,13 @@
 							service.getData(matches).then(function (datas) {
 								resolve(datas);
 							}, function (error) {
-								reject("error while getting data : ", error);
+								reject(error);
 							});
 						}, function (error) {
-							reject("error while getting matches : ", error);
+							reject(error);
 						});
 					}, function (error) {
-						reject("error while getting user's hash : ", error);
+						reject(error);
 					});
 				}));
 			}
@@ -191,7 +250,7 @@
 		$scope.service = scrap;
 		$scope.data = [];
 		$scope.user = {};
-		$scope.error = 0;
+		$scope.service.errormsg = false;
 		$scope.filter = {
 			username: '',
 			orderBy: 'nickname',
@@ -211,7 +270,8 @@
 				console.log("stats : ", datas);
 			}, function(error) {
 				$scope.$apply(function() {
-					$scope.error = "Error. Please try again."
+					console.log(error);
+					$scope.service.errormsg = error.status + " : " + error.data.message;
 				});
 			});
 		};
